4620a14cbd52e73d4fd5f789034690df0dd5e6a2
sandra18-nov <sandramugucia@gmail.com>
2023-02-16 09:25:59 +0300

    hello message

---
+++ game.py
@@ -0,0 +1,32 @@
+#1 get user name 
+#from random import randint
+number=randint(10, 50)
+username=input("enter name: ")
+print("hello there" + username + "!")
+
+#generate a number
+counter=0
+while counter=5;
+ usernumber=eval(input("enter number:"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#usig a while loop
+#get user number
+#
+#check if user input is equal to generated number
